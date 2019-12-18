fc1c70bb37ac4c831ed63ee112bc27dc8721d84f
Brijesh Maurya <brijesh@sodainmind.com>
2019-12-18 18:35:27 +0530

    Backend user data

--- application/controllers/Superadmin.php
+++ application/controllers/Superadmin.php
@@ -125,25 +125,124 @@ public function password_sendMail()
     $get = $this->db->where('email',$email)
                          ->from('superadmin')
                          ->get();
-    $getemail = $get->result_array();  
-    $user_id = $getemail[0]['id'];
-    print_r($user_id);                   
+    $getemail = $get->result_array();                  
     if(count($getemail)==1)
     {
+
+    $user_id = $getemail[0]['id']; 	
+    $to = $getemail[0]['email'];
+    $subject = 'Password Reset Mail';
+    $from = 'brijesh@sodainmind.com';
+    // set email content
+    $emailContent = '<!DOCTYPE><html><head></head><body><table width="600px" style="border:1px solid 
+                    #cccccc;margin: auto;border-spacing:0;"><tr><td
+                    style="background:#ffffff;padding-left:3%">Ems-Superadmin</td></tr>';
+    $emailContent.='<tr><td style="height:20px"></td></tr>';
+    $emailContent.='<tr><td style="height:20px"></td></tr>';
+    $emailContent.='<tr><td style="height:20px"></td></tr>';                
+    $emailContent.= "<tr><td style='background:#000000;color: #ffffff;padding: 2%;text-align: center;font-size:
+                    13px;'><p style='margin-top:1px;'>Ems Superadmin</p></td></tr></table></body></html>";
+    
+    // configure for send mail 
+    $config['protocol']    = 'smtp';
+    $config['smtp_host']    = 'smtp.sendgrid.net';
+    $config['smtp_port']    = '587';
+    $config['smtp_timeout'] = '60';
+
+    $config['api_user']   = 'Brijesh_Maurya';    //Important
+    $config['api_key']    = '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';  //Important
+    $config['api_format'] = 'json';
+
+    $config['charset']    = 'utf-8';
+    $config['newline']    = "\r\n";
+    $config['mailtype'] = 'html'; // or html
+    $config['validation'] = TRUE; // bool whether to validate email or not 
+
+     
+
+    $this->email->initialize($config);
+    $this->email->set_mailtype("html");
+    $this->email->from($from);
+    $this->email->to($to);
+    $this->email->subject($subject);
+    $this->email->message($emailContent);
+    $this->email->send();
+
+    $this->session->set_flashdata('msg',"Mail has been sent successfully");
+    $this->session->set_flashdata('msg_class','alert-success');
+    return redirect(base_url('superadmin/forget_password'));
     	
     }
     else
     {
     	echo "You have enter incorrect email ";
     }	
-        
-
     
+    } // end if
 
-	}
 
+} // end of send mail function
+
+// backend user controlloer
+public function load_backend_user()
+{
+	$this->load->view('backenduser_insert');
 
 }
+
+public function data_insert()
+{
+	if($this->input->post('submit'))
+	{
+	$data['school_id'] = $this->input->post('school_id');
+	$data['role'] = $this->input->post('role');
+	$data['login_name'] = $this->input->post('login_name');
+	$data['password']= md5($this->input->post('password'));
+	$data['first_name'] = $this->input->post('first_name');
+	$data['last_name'] = $this->input->post('last_name');
+    $data['user_email'] = $this->input->post('user_email');
+    $data['user_phone'] = $this->input->post('user_phone');
+    $data['user_mobile'] = $this->input->post('user_mobile');
+    $data['user_dob'] = $this->input->post('user_dob');
+    $data['user_doj'] = $this->input->post('user_doj');
+    $insert = $this->superadmin->insert_backenduser($data);
+    if($insert)
+	{
+        $this->session->set_flashdata('message',"Your Registration Successfull");
+        $this->load->view('backenduser_insert');
+	}
+	
+    }
+}
+
+// check user exit in backend user table
+public function Checkemail_userbackend()
+{
+	$requestedEmail = $this->input->post('user_email');
+	$getemail=$this->db->get_where('wp_backend_user',array('user_email' => $requestedEmail))->num_rows();
+	if($getemail == 0)
+	{
+		echo 'true';
+	}
+	else
+	{
+		echo 'false';
+	}
+
+}
+
+
+// dbackend user data edit
+public function show_backenduser()
+{
+	$this->load->view('show_backenduser');
+}
+
+public function edit_data()
+{
+	$this->load->view('edit_data');
+}
+
 
 
 
