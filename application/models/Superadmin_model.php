6bbf2dfde1f868f700864f91166561c9d65a4d1a
Brijesh Maurya <brijesh@sodainmind.com>
2019-12-18 09:46:03 +0530

    admin login model

--- application/models/Superadmin_model.php
+++ application/models/Superadmin_model.php
@@ -1,9 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/**
- * 
- */
+
 class Superadmin_model extends CI_model
 {
 	
@@ -12,6 +10,22 @@ class Superadmin_model extends CI_model
 	{
 	  return $this->db->insert('superadmin',$data);
 	} 
+
+// code for login
+	public function admin_login($username,$password)
+          {
+		        $q = $this->db->where('username',$username)
+		                      ->where('password',$password)
+		                      ->from('superadmin')
+		                      ->get();     
+		       if($q->num_rows() == 1) 
+		       {
+           		 return $q->row();
+        		}
+
+        		return false;
+ 
+          }
 
 
 
