edc283ad5fe5cc30ae6e3c170f7472210e85c786
Brijesh Maurya <brijesh@sodainmind.com>
2019-12-18 10:28:13 +0530

    Create Forget Password Link

--- application/views/index.php
+++ application/views/index.php
@@ -21,21 +21,22 @@
                     <div class="text-center p-t-20 p-b-20">
                         <span class="db" style="color: white;font-size: 20px;">Login Admin</span>
                     </div>
+                     <span style="color: white;font-size: 15px;"><?php echo $this->session->flashdata('login_failed'); ?></span>
                     <!-- Form -->
-                    <form class="form-horizontal m-t-20" id="loginform" action="superadmin/login">
+                    <form class="form-horizontal m-t-20" id="loginform" method="post" action="<?php echo base_url();?>superadmin/superadmin_login">
                         <div class="row p-b-30">
                             <div class="col-12">
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text bg-success text-white" id="basic-addon1"><i class="ti-user"></i></span>
                                     </div>
-                                    <input type="text" name="username" class="form-control form-control-lg" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" required="">
+                                    <input type="text" name="username" class="form-control form-control-lg" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
                                 </div>
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text bg-warning text-white" id="basic-addon2"><i class="ti-pencil"></i></span>
                                     </div>
-                                    <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" required="">
+                                    <input type="password" name="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
                                 </div>
                             </div>
                         </div>
@@ -43,56 +44,19 @@
                             <div class="col-12">
                                 <div class="form-group">
                                     <div class="p-t-20">
-                                        <button class="btn btn-info" id="to-recover" type="submit"><i class="fa fa-lock m-r-5"></i> Lost password?</button>
-                                        <button class="btn btn-success float-right" type="submit">Login</button>
-                                        <br><span style="color: white;"><a href="<?php echo base_url();?>superadmin/register">click here if you are not registered yet</a></span>
+                                    <button class="btn btn-success" type="submit" name="submit">Login</button>
+                                    <span style="color: white;"><a href="<?php echo base_url();?>superadmin/forget_password" class="btn btn-info float-right">Forget Password</a></span>
+                                    <br><span style="color: white;"><a href="<?php echo base_url();?>superadmin/register">click here if you are not registered yet</a></span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </form>
-                </div>
-                <div id="recoverform">
-                    <div class="text-center">
-                        <span class="text-white">Enter your e-mail address below and we will send you instructions how to recover a password.</span>
-                    </div>
-                    <div class="row m-t-20">
-                        <!-- Form -->
-                        <form class="col-12" action="index.html">
-                            <!-- email -->
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text bg-danger text-white" id="basic-addon1"><i class="ti-email"></i></span>
-                                </div>
-                                <input type="text" class="form-control form-control-lg" placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1">
-                            </div>
-                            <!-- pwd -->
-                            <div class="row m-t-20 p-t-20 border-top border-secondary">
-                                <div class="col-12">
-                                    <a class="btn btn-success" href="#" id="to-login" name="action">Back To Login</a>
-                                    <button class="btn btn-info float-right" type="button" name="action">Recover</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                   
                 </div>
             </div>
         </div>
-        <!-- ============================================================== -->
-        <!-- Login box.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Page wrapper scss in scafholding.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Page wrapper scss in scafholding.scss -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Right Sidebar -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Right Sidebar -->
-        <!-- ============================================================== -->
+
     </div>
 
     <script>
