144d3f7c68b44003b520777de1c0d87f96a423c5
Brijesh Maurya <brijesh@sodainmind.com>
2019-12-19 18:21:52 +0530

    add school name check function

--- application/views/insertSchool.php
+++ application/views/insertSchool.php
@@ -2,16 +2,12 @@
 <?php include 'rightmenu.php'; ?>
 
  <div class="page-wrapper">
-            <!-- ============================================================== -->
-            <!-- Bread crumb and right sidebar toggle -->
-            <!-- ============================================================== -->
-            <div class="page-breadcrumb">
-                <div class="row">
+    <div class="page-breadcrumb">
+       <div class="row">
                   
-                </div>
-            </div>
+        </div>
+    </div>
             <div class="container-fluid">
-
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
@@ -22,7 +18,7 @@
                                     <div class="form-group row">
                                         <label for="cono1" class="col-sm-3 text-right control-label col-form-label">School Name</label>
                                         <div class="col-sm-6">
-                                            <input type="text" class="form-control" name="school_name" id="school_name" placeholder="School Name" value="" required />
+                                            <input type="text" class="form-control" name="school_name" id="school_name" placeholder="School Name" value="" />
                                         </div>
                                     </div>
                                 </div>
@@ -39,6 +35,39 @@
  
             </div>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script>
+$(document).ready(function(){
+$('#signup-form').validate({
+
+     rules: {
+                    school_name: {
+                        required : true,
+                          remote: {
+                                url: "<?php echo base_url();?>superadmin/CheckschoolName",
+                                type: "post"   
+                            }
+
+                    },
+
+
+
+                }, // end of rules
+ messages: {
+              
+                    school_name: {
+                        required : "School Name must is required",
+                        remote: "School Name is already exit."
+                    },
+                 
+         } // message tag end        
+});     
+
+});
+    
+</script>
+
 
 
 <?php include 'footer.php'; ?>
